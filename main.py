cb02c61968332afd2d5de4c3ed6ffc8b4c3c7028
bimarshak7 <khanalbimarsha7@gmail.com>
2021-11-28 21:42:34 +0545

    Requirements fixed

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = Flask(__name__,template_folder='static/front')
 app.config['SECRET_KEY'] = os.environ['secret_KEY']
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instapound.db'
+DATABASE_URL = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@localhost:5432/instapound"
 db=SQLAlchemy(app)
